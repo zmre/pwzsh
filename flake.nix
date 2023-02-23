6dd302167e502dc5468745a8520a2062232c3fd8
Patrick Walsh <patrick.walsh@ironcorelabs.com>
2023-02-23 08:42:06 -0700

    Updates

--- flake.nix
+++ flake.nix
@@ -6,74 +6,79 @@
     flake-utils.url = "github:numtide/flake-utils";
     pwnvim.url = "github:zmre/pwnvim";
   };
-  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in rec {
-        dependencies = with pkgs;
-          [
-            bat
-            btop
-            coreutils
-            cowsay # this is here to help with testing
-            curl
-            direnv
-            exa
-            exif
-            fd
-            file
-            fzf
-            fzy
-            git
-            gnused
-            less
-            mdcat # colorize markdown
-            pstree
-            inputs.pwnvim.packages.${system}.pwnvim
-            ripgrep
-            starship
-            zoxide
-            zsh
-            zsh-completions
-            zsh-autocomplete
-            zsh-autosuggestions
-            zsh-syntax-highlighting
-          ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [ ueberzug ];
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in rec {
+      dependencies = with pkgs;
+        [
+          bat
+          btop
+          coreutils
+          cowsay # this is here to help with testing
+          curl
+          direnv
+          exa
+          exif
+          fd
+          file
+          fzf
+          fzy
+          git
+          gnused
+          less
+          mdcat # colorize markdown
+          pstree
+          inputs.pwnvim.packages.${system}.pwnvim
+          ripgrep
+          starship
+          zoxide
+          zsh
+          zsh-completions
+          zsh-autocomplete
+          zsh-autosuggestions
+          zsh-syntax-highlighting
+        ]
+        ++ pkgs.lib.optionals pkgs.stdenv.isLinux [ueberzug];
 
-        packages.pwzsh = with pkgs;
-          stdenv.mkDerivation rec {
-            version = "1.0.0";
-            name = "pwzsh";
-            inherit system;
-            src = self;
-            buildInputs = [ zsh ] ++ dependencies;
-            nativeBuildInputs = [ makeWrapper ];
-            phases = [ "installPhase" ];
-
-            installPhase = ''
-              mkdir -p "$out/bin"
-              mkdir -p "$out/config"
-              export zsh_autosuggestions="${pkgs.zsh-autosuggestions}"
-              export zsh_autocomplete="${pkgs.zsh-autocomplete}"
-              export zsh_syntax_highlighting="${pkgs.zsh-syntax-highlighting}"
-              export fzf="${pkgs.fzf}"
-              export starship="${pkgs.starship}"
-              export zoxide="${pkgs.zoxide}"
-              export direnv="${pkgs.direnv}"
-              export zsh_completions="${pkgs.zsh-completions}"
-              substituteAll $src/config/zshrc $out/config/.zshrc
-              makeWrapper "${zsh}/bin/zsh" "$out/bin/pwzsh" --set SHELL_SESSIONS_DISABLE 1 --set ZDOTDIR "$out/config" --set PWZSH 1 --prefix PATH : "$out/bin:"${
-                pkgs.lib.makeBinPath dependencies
-              }
-            '';
-          };
-        apps.pwzsh = flake-utils.lib.mkApp {
-          drv = packages.pwzsh;
+      packages.pwzsh = with pkgs;
+        stdenv.mkDerivation rec {
+          version = "1.0.0";
           name = "pwzsh";
-          exePath = "/bin/pwzsh";
-        };
-        packages.default = packages.pwzsh;
-        apps.default = apps.pwzsh;
-      });
+          inherit system;
+          src = self;
+          buildInputs = [zsh] ++ dependencies;
+          nativeBuildInputs = [makeWrapper];
+          phases = ["installPhase"];
 
+          installPhase = ''
+            mkdir -p "$out/bin"
+            mkdir -p "$out/config"
+            export zsh_autosuggestions="${pkgs.zsh-autosuggestions}"
+            export zsh_autocomplete="${pkgs.zsh-autocomplete}"
+            export zsh_syntax_highlighting="${pkgs.zsh-syntax-highlighting}"
+            export fzf="${pkgs.fzf}"
+            export starship="${pkgs.starship}"
+            export zoxide="${pkgs.zoxide}"
+            export direnv="${pkgs.direnv}"
+            export zsh_completions="${pkgs.zsh-completions}"
+            substituteAll $src/config/zshrc $out/config/.zshrc
+            makeWrapper "${zsh}/bin/zsh" "$out/bin/pwzsh" --set SHELL_SESSIONS_DISABLE 1 --set ZDOTDIR "$out/config" --set PWZSH 1 --prefix PATH : "$out/bin:"${
+              pkgs.lib.makeBinPath dependencies
+            }
+          '';
+        };
+      apps.pwzsh = flake-utils.lib.mkApp {
+        drv = packages.pwzsh;
+        name = "pwzsh";
+        exePath = "/bin/pwzsh";
+      };
+      packages.default = packages.pwzsh;
+      apps.default = apps.pwzsh;
+    });
 }
